09549ae65bd2fda8cad28865d93e48508ea5b39c
Rafa7882 <rafacho_montanez@aol.com>
2020-09-04 19:55:43 -0300

    'segundo'

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructura_de_Datos___Tarea_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tarea2;          
+            Console.WriteLine(200 + 56);
+            Console.ReadLine();
+            */
+
+            /*Tarea3;            
+            Console.WriteLine(13 * 12);
+            Console.ReadLine();
+            */
+
+            /*Tarea4;            
+            Console.WriteLine(321 - 213);
+            Console.ReadLine();
+            */
+
+            /*Tarea5;            
+            Console.WriteLine((20+5) % 6);
+            Console.ReadLine();
+            */
+
+            /*Tarea6;
+            int numA = 12;
+            int numB = 102;
+            Console.WriteLine(numA * numB);
+            Console.ReadLine();
+            */
+
+            /*Tarea7;
+            int numA = 200;
+            int numB = 1111;
+            Console.WriteLine(numA + numB);
+            Console.ReadLine();
+            */
+
+            /*Tarea8;
+            int numero1 = 10;
+            int numero2 = 25;
+            Console.WriteLine(numero1 * numero2);
+            Console.ReadLine();
+            */
+
+            /*Tarea9;
+            int num1;
+            Console.WriteLine("Introduce un Número");       
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int num2;
+            Console.WriteLine("Introduce un Número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 * num2);
+            Console.ReadLine();
+            */
+
+            /*Tarea10;
+            int num1;
+            Console.WriteLine("Introduce un Número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 == 0) 
+                Console.WriteLine("Tu número es Par");
+            else
+                Console.WriteLine("Tu número es Impar");
+            Console.ReadLine();
+            */
+
+            /*Tarea11;
+            int num1;
+            Console.WriteLine("Introduce un Número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int num2;
+            Console.WriteLine("Introduce otro Número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int mayor;
+            mayor = num1 > num2 ? num1 : num2;
+            Console.WriteLine("El número mayor es " + mayor);
+            Console.ReadLine();
+            */
+
+            /*Tarea12;
+            int numeroA;
+            Console.WriteLine("Introduce el número dividendo");
+            numeroA = Convert.ToInt32(Console.ReadLine());
+            int numeroB;
+            Console.WriteLine("Introduce el número divisor");
+            numeroB = Convert.ToInt32(Console.ReadLine());
+            if (numeroB != 0)
+                Console.WriteLine(numeroA / numeroB);
+            else
+                Console.WriteLine("Error, No se puede dividir entre 0");
+            Console.ReadLine();
+            */
+
+            /*Tarea13;
+            int num1;
+            Console.WriteLine("Escribe un numero del 1 al 5");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            switch (num1)
+            {
+                case 1: Console.WriteLine("Uno");
+                Console.ReadLine();
+                break;
+                case 2: Console.WriteLine("Dos");
+                Console.ReadLine();
+                break;
+                case 3: Console.WriteLine("Tres");
+                Console.ReadLine();
+                break;
+                case 4: Console.WriteLine("Cuatro");
+                Console.ReadLine();
+                break;
+                case 5: Console.WriteLine("Cinco");
+                Console.ReadLine();
+                break;
+                default: Console.WriteLine("No es un número solicitado");
+                Console.ReadLine();
+                break;
+            }
+            */
+
+            /*Tarea14;
+            int n = 1;
+            while (n < 11)
+            {
+                Console.WriteLine(n);
+                n = n + 1;
+            }
+            Console.ReadLine();
+            */
+
+            /*Tarea15;
+            int n = 1;
+            do
+            {
+                Console.WriteLine(n);
+                n = n + 1;
+            }
+            while (n < 11);
+            Console.ReadLine();
+            */
+
+            /*Tarea16;
+            int i;
+            for (i = 10; i <= 20; i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+            */
+
+            int i;
+            for (i=0; i<7; i++)
+            {
+                Console.WriteLine("Hola Mundo");
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+            }
+    }
+}
